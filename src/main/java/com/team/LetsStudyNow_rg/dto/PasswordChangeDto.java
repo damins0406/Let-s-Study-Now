1ed59606201341855331d3a8f4aab67ca9372961
vkfro2293 <vkfro2293@naver.com>
2025-10-30 21:09:16 +0900

    refector: memberService, RegisterDto, PasswordChange 설명 주석 추가

--- src/main/java/com/team/LetsStudyNow_rg/dto/PasswordChangeDto.java
+++ src/main/java/com/team/LetsStudyNow_rg/dto/PasswordChangeDto.java
@@ -8,6 +8,7 @@ public record PasswordChangeDto(
         @NotBlank(message = "현재 비밀번호를 입력해주세요.")
         String currentPassword,
 
+        // 비밀번호는 영문자, 숫자, 특수기호를 반드시 포함해야 한다.
         @NotBlank @Size(min = 6, max = 15, message = "비밀번호는 6~15자로 작성해주세요.")
         @Pattern(
                 regexp="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]+$",
--- src/main/java/com/team/LetsStudyNow_rg/dto/RegisterDto.java
+++ src/main/java/com/team/LetsStudyNow_rg/dto/RegisterDto.java
@@ -5,14 +5,17 @@ import jakarta.validation.constraints.*;
 public record RegisterDto(
         @NotBlank @Email
         String email,
+
         @NotBlank @Size(min = 2, max = 12, message = "아이디는 2~12자 내로 작성해주세요.")
         String username,
+        // 비밀번호는 영문자, 숫자, 특수기호를 반드시 포함해야 한다.
         @NotBlank @Size(min = 6, max = 15, message = "비밀번호는 6~15자로 작성해주세요.")
         @Pattern(
                 regexp="^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]+$",
                 message = "비밀번호는 영문자, 숫자, 특수기호(@$!%*#?&)를 포함해야 합니다."
         )
         String password,
+
         @NotBlank
         String checkPassword,
 
