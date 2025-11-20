581328abd8227ace41ea2acd9484bde8a58b1674
vkfro2293 <vkfro2293@naver.com>
2025-11-20 20:57:52 +0900

    develop: s3 연결, 프로필 업로드 기능 구현

--- src/main/java/com/team/LetsStudyNow_rg/domain/member/service/MemberService.java
+++ src/main/java/com/team/LetsStudyNow_rg/domain/member/service/MemberService.java
@@ -11,8 +11,10 @@ import com.team.LetsStudyNow_rg.domain.member.exception.MemberNotFoundException;
 import com.team.LetsStudyNow_rg.domain.member.repository.MemberRepository;
 import com.team.LetsStudyNow_rg.global.auth.CustomUser;
 import com.team.LetsStudyNow_rg.global.jwt.JwtUtil;
+import com.team.LetsStudyNow_rg.global.s3.S3Service;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -21,12 +23,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final S3Service s3Service;
+
+    // 기본 프로필 url
+    @Value("${custom.s3.default-image-url}")
+    private String defaultProfileImageUrl;
+
     final int ACCESS_TIME = 60 * 60;
 
     // 로그인 로직
@@ -62,12 +72,14 @@ public class MemberService {
 
         String encodePw = passwordEncoder.encode(req.password());
 
+        String profileImageUrl = defaultProfileImageUrl;
+
         Member member = Member.builder()
                 .username(req.username())
                 .email(req.email())
                 .password(encodePw)
                 .role(Role.ROLE_USER)
-                // .profileImage(req.profileImageFile())
+                .profileImage(profileImageUrl)
                 .studyField(req.studyField())
                 .bio(req.bio())
                 .build();
