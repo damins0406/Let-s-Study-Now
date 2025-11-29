8fbe80776332929d488d22677341a7d7143dcb37
LAPTOP-F4BLFDGR\kimjuyoung <juyoung012684@gmail.com>
2025-11-29 19:36:53 +0900

    ProfileDto에 id 필드 추가

--- src/main/java/com/team/LetsStudyNow_rg/domain/member/dto/response/ProfileDto.java
+++ src/main/java/com/team/LetsStudyNow_rg/domain/member/dto/response/ProfileDto.java
@@ -1,6 +1,7 @@
 package com.team.LetsStudyNow_rg.domain.member.dto.response;
 
 public record ProfileDto(
+        Long id,
         String email,
         String username,
         String profileImage,
